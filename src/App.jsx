25deb84268bf5c1c01bbbbfae97582d39108a62f
shubham11602152 <shubhammehta.333@gmail.com>
2023-03-11 22:29:42 +0530

    [Refactored] project file cleaning

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "antd/dist/reset.css";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
